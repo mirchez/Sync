d4685d749e96e93ade1e5939af93083544f9c159
Miguel Miranda <mmirandasanchez16@gmail.com>
2025-07-17 15:52:40 -0300

    Update src/features/auth/components/sign-up-card.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -120,7 +120,7 @@ export const SignUpCard = () => {
             />
 
             <Button disabled={false} size="lg" className="w-full">
-              Login
+              Sign Up
             </Button>
           </form>
         </Form>
